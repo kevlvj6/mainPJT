73689451e4bb6cbfe4624fee2e1693503a653ac5
Zino <jinhojeon5568@gmail.comgit config --global user.namgit config --global user.name Zinogit config --global user.email jinhojeon5568@gmail.com>
2023-12-19 16:43:40 +0900

    새로운 브ãCCTV 페이지 수ì정 완료

--- rootWEB/mainApp/urls.py
+++ rootWEB/mainApp/urls.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
-from mainApp     import views
+from mainApp import views
 
 urlpatterns = [
     # http://127.0.0.1:8000/
     path("", views.login),
     path("main/", views.main),
-    path("CCTV/" , views.CCTV),
+    path("CCTV/", views.CCTV),
+    path("QNA/", views.QNA),
+    path("detectme/", views.detectme),
 ]
--- rootWEB/mainApp/views.py
+++ rootWEB/mainApp/views.py
@@ -1,16 +1,64 @@
+
 from django.http import JsonResponse
 from django.shortcuts import render, redirect
 
+from django.views.decorators import gzip
+from django.http import StreamingHttpResponse
+import cv2
+import threading
+
 # Create your views here.
 
-def login(request) :
+def login(request):
     print('debug >>>> mainApp /login')
     return render(request, 'main/login.html')
 
-def main(request) :
+
+def main(request):
     print('debug >>>> mainApp /main')
     return render(request, 'main/main.html')
 
-def CCTV(request) :
+
+def CCTV(request):
     print('debug >>>> mainApp /CCTV')
     return render(request, 'main/CCTV.html')
+
+
+def QNA(request):
+    print('debug >>>> mainApp /QNA')
+    return render(request, 'main/QNA.html')
+
+
+class VideoCamera(object):
+    def __init__(self):
+        self.video = cv2.VideoCapture(0)
+        (self.grabbed, self.frame) = self.video.read()
+
+        threading.Thread(target=self.update, args=()).start()
+
+    def __del__(self):
+        self.video.release()
+
+    def get_frame(self):
+        image = self.frame
+        _, jpeg = cv2.imencode('.jpg', image)
+        return jpeg.tobytes()
+
+    def update(self):
+        while True:
+            (self.grabbed, self.frame) = self.video.read()
+
+def gen(camera):
+    while True:
+        frame = camera.get_frame()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+
+@gzip.gzip_page
+def detectme(request):
+    try:
+        cam = VideoCamera()  # VideoCamera 인스턴스 생성
+        return StreamingHttpResponse(gen(cam), content_type="multipart/x-mixed-replace;boundary=frame")
+    except:
+        print("에러입니다")
+        pass
